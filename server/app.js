bf39b452d22ba1cd3dbc21a9e48de008b847e76a
Woody Lewis <woody@woodylewis.com>
2024-01-18 17:32:29 -0500

    Update app.js

--- server/app.js
+++ server/app.js
@@ -15,7 +15,7 @@ app.use("/", organizationRouter);
 app.use("/", transactionRouter);
 
 const PORT = 4000;
-const MONGO_URI = 'mongodb+srv://wls-admin:bzyuOslrQ3NKkp0H@cluster0.zumlq40.mongodb.net/unicef';
+const MONGO_URI = '';
 
 mongoose
 .connect(MONGO_URI)
